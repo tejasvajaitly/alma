caa69a71954283a14af44bd4795bdace961f229f
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-07-20 19:44:04 -0700

    highlight current route on sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,31 @@
-export default function Layout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import React, { ReactNode } from "react";
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <div className="flex">
-      <div className="flex-[0.2]">sidebar</div>
-      <div className="flex-[0.8]">{children}</div>
+    <div className="flex flex-row">
+      <aside className="flex-[0.2]">
+        <nav>
+          <Link
+            href="/dashboard/"
+            className={pathname === "/dashboard" ? "bg-slate-600" : ""}
+          >
+            Leads
+          </Link>
+          <Link
+            href="/dashboard/settings"
+            className={pathname === "/dashboard/settings" ? "bg-slate-600" : ""}
+          >
+            Settings
+          </Link>
+        </nav>
+      </aside>
+      <main className="flex-[0.8]">{children}</main>
     </div>
   );
 }
