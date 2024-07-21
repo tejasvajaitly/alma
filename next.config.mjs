f8add0f395a4bbe8ead920cd9667fcf768d98f65
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-07-21 07:01:01 -0700

    changes made to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-module.exports = {
+const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
+
+module.exports = nextConfig;
